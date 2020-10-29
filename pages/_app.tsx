1b093e9ad85819e9cbcdb85faf639c5fbaa38c69
Chris Otto <chris.otto6@gmail.com>
2020-10-29 12:49:21 -0500

    Adding more start config for eslint, prettier, etc

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import { AppProps } from 'next/app'
+
+import '../global.css'
+
+const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+)
+
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
 const IndexPage: FC = () => {
-  return <h1>Hello, user!</h1>
+  return <h1 className='text-black-700'>Hello, user!</h1>
 }
 
 export default IndexPage
